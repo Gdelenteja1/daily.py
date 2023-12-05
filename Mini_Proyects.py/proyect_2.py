3f1c32b6bc146d877202ba4cc018c470c42fee2a
IgnacioMPpalleiro <ignaciompalleiro@gmail.com>
2023-12-05 15:25:44 -0300

    Quiz Game

---
+++ Mini_Proyects.py/proyect_2.py
@@ -0,0 +1,11 @@
+# QUIZ GAME
+
+
+print("¡Welcome to the Quiz game!")
+
+
+welcoming = input("Are you ready for the Quiz Game? (y)\n")
+if welcoming.lower() == "y":
+    print("Good! Lets Begin.")
+else:
+    print("Invalid, try again.")
